00435155d9fe4ffa874bab7d5434232d2b43378f
agalemmo <galem100@mail.chapman.edu>
2018-05-07 20:15:57 -0700

    Create Flashlight.cs

---
+++ Flashlight.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    //when flashlight is attached to player (look to code regarding when character in platform picks up gun) becomes a child of playercharacter
+    // Use this for initialization
+    void Awake()
+    {
+        transform.parent = false;
+    }
+
+    //once triggered transform.parent = true;
+    //once triggered release the sprite & collider boxes go away.
+}
