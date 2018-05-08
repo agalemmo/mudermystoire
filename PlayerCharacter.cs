4230ca8f0ffdd1fb5f56c1f3072ddca584801bd3
agalemmo <galem100@mail.chapman.edu>
2018-05-07 19:36:48 -0700

    uploaded PlayerCharacter.cs

---
+++ PlayerCharacter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+
+    public float playerSpeed = 1f;
+
+    private Vector2 playerPos = new Vector2(0, 0);
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xPos = transform.position.x + (Input.GetAxis("Horizontal")) * paddleSpeed;
+        playerPos = new Vector2(Mathf.Clamp(xPos, -2.84f, 2.84f), -1.8f);
+        transform.position = playerPos;
+    }
+}
